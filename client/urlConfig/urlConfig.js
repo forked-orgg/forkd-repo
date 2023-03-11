50ddd161cba127647da39b89222f656e64301016
marwenslimen <marwaneslimane@gmail.com>
2023-03-11 11:01:21 +0100

    test1

---
+++ client/urlConfig/urlConfig.js
@@ -0,0 +1,7 @@
+
+export const baseUrl = "http://192.168.175.170:3000/";
+
+// export const VerifyRegUrl  = baseUrl + "api/verifyy"
+// export const FetchHierarchyUrl = baseUrl + "api/hierarchy/employees/";
+// export const moveWorkerUrl = baseUrl + "api/hierarchy/moving/";
+// export const loginUrl = baseUrl + 'api/login';
